9764e6dd7167776381ec7a4a963157ca25b99bf6
chumchek <kaliuzhnyi.ivan@lll.kpi.ua>
2024-08-12 16:07:08 +0300

    first commit

---
+++ vars.tf
@@ -0,0 +1,40 @@
+variable "REGION" {
+  default = "us-east-1"
+}
+
+variable "ZONE1" {
+  default = "us-east-1a"
+}
+
+variable "ZONE2" {
+  default = "us-east-1b"
+}
+
+variable "ZONE3" {
+  default = "us-east-1c"
+}
+
+variable "AMIS" {
+  type = map(any)
+  default = {
+    us-east-1 = "ami-03972092c42e8c0ca"
+    us-east-2 = "ami-0a31f06d64a91614b"
+  }
+}
+
+variable "USER" {
+  default = "ec2-user"
+}
+
+variable "PUB_KEY" {
+  default = "dovekey.pub"
+}
+
+variable "PRIV_KEY" {
+  default = "dovekey"
+}
+
+variable "MYIP" {
+  default = "62.80.182.184/32"
+}
+
